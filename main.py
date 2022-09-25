b837664009f635afb1961dcc3536b0350b374691
Charin Ivan <lumidelta07@gmail.com>
2022-09-25 22:36:35 +0300

    Added function to add record for user

--- main.py
+++ main.py
@@ -7,6 +7,15 @@ bot = telebot.TeleBot(token)
 def send_welcome(message):
 	bot.reply_to(message, "Howdy, how are you doing?")
 
+@bot.message_handler(commands=['add'])
+def add(message):
+    user = bot.get_me()
+    file = open('data/' + str(message.chat.id) + '.csv', 'a+')
+    file.write("Hello,hello,hello\n")
+    file.close()
+
+    bot.send_message(message.chat.id, "What do you want to add?")
+
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
 	bot.reply_to(message, message.text)
